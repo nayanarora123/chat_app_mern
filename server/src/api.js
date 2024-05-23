41bec09b2da0b9e9ef2c27419ba554d237d56aa5
nayanarora123 <aroranayan3@gmail.com>
2024-05-23 22:16:14 +0530

    env

--- server/src/api.js
+++ server/src/api.js
@@ -21,16 +21,16 @@ api.use('/api/user', userRouter);
 api.use('/api/room', chatRoomRouter);
 api.use('/api/message', chatMessageRouter);
 
-if (process.env.NODE_ENV === "production") {
-    const __dirname = import.meta.dirname;
-    api.use(express.static(path.join(__dirname, 'public')));
-    api.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'), (err)  => {
-            if(err) {
-                res.status(500).send({error: err, path: __dirname, cwd: process.cwd()});
-            }
-        });
-    })
-}
+// if (process.env.NODE_ENV === "production") {
+//     const __dirname = import.meta.dirname;
+//     api.use(express.static(path.join(__dirname, 'public')));
+//     api.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname, 'public', 'index.html'), (err)  => {
+//             if(err) {
+//                 res.status(500).send({error: err, path: __dirname, cwd: process.cwd()});
+//             }
+//         });
+//     })
+// }
 
 export default api;
