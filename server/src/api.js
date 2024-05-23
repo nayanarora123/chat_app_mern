32aaa82953c54d559d6308f934b7adad21ef2b25
nayanarora123 <aroranayan3@gmail.com>
2024-05-23 21:26:21 +0530

    path upadted

--- server/src/api.js
+++ server/src/api.js
@@ -22,14 +22,12 @@ api.use('/api/room', chatRoomRouter);
 api.use('/api/message', chatMessageRouter);
 
 if (process.env.NODE_ENV === "production") {
-    // const __dirname = import.meta.dirname;
-    // const __filename = fileURLToPath(import.meta.url);
-    // const __dirname = path.dirname(__filename);
-    api.use(express.static(path.join(process.cwd(), '..', 'public')));
+    const __dirname = path.dirname(__filename);
+    api.use(express.static(path.join(__dirname, '..', 'public')));
     api.get("*", (req, res) => {
-        res.sendFile(path.join(process.cwd(), '..', 'public', 'index.html'), (err)  => {
+        res.sendFile(path.join(__dirname, '..', 'public', 'index.html'), (err)  => {
             if(err) {
-                res.status(500).send(err)
+                res.status(500).send({error: err, path: __dirname, cwd: process.cwd()})
             }
         });
     })
