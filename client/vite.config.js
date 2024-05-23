ad79e9fc7fee250d4486d36de3773c83ea230d07
shikha <shikhawebworks@gmail.com>
2024-05-23 17:15:23 +0530

    server update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   outDir: './', 
-  // },
-  // server: {
-  //   port: 3000,
-  //   host: true,
-  // }
+  build: {
+    outDir: '../server/public', 
+  },
+  server: {
+    port: 3000,
+    host: true,
+  }
 });
 
 
